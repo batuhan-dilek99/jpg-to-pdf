38b7ea7b326f3c68093762ba7a099d87e04392ff
batuhan-dilek99 <80598580+batuhan-dilek99@users.noreply.github.com>
2021-08-27 19:47:36 +0300

    Add files via upload

--- converter/convert.py
+++ converter/convert.py
@@ -1,6 +1,9 @@
 from PIL import Image
 
-def jpgToPdf(image_name) : 
-    jpgImage = Image.open(r'C:/Users/Batuhan/Desktop/converter/jpgimage.jpg')
-    pdfImage = jpgImage.convert('RGB')
-    pdfImage.save(r'C:/Users/Batuhan/Desktop/converter/pdfImage.pdf')
+def jpgToPdf(image_path, pdf_path) : 
+    try:
+        jpgImage = Image.open(image_path + '/jpgimage.jpg')
+        pdfImage = jpgImage.convert('RGB')
+        pdfImage.save(pdf_path + '/pdfImage.pdf')
+    except:
+        print("Could not load the image")
--- converter/main.py
+++ converter/main.py
@@ -1,13 +1,30 @@
 import bt
 import convert
 import cv2
-path = input("Please enter the path of the image : ")
-image_name = input("Please enter the image name (imagename.jpg) : ")
-image_name = '/' + image_name
-image = cv2.imread(path + image_name)
-image_thresh = bt.blurNthresh(image)
-cv2.imwrite(path + "/jpgimage.jpg", image_thresh)
-convert.jpgToPdf(path + '/jpgimage.jpg')
+import os
+
+
+try:
+    path = input("Please enter the path of the file : ")
+    image_name = input("Please enter the image name (imagename.jpg) : ")
+    pdf_path = path + "/pdfs"
+    image_path = path + "/images"
+except:
+    print("There is no such path or image. Check and try again")
+
+
+try:
+    image_name = '/' + image_name
+    image = cv2.imread(image_path + image_name)
+    image_thresh = bt.blurNthresh(image)
+    cv2.imwrite(path + "/images/jpgimage.jpg", image_thresh)
+    convert.jpgToPdf(image_path, pdf_path)
+    print(image_name[1:30] + " has successfully converted to pdf at : " + pdf_path)
+    try:
+        os.remove(image_path + "/jpgimage.jpg")
+    except : pass
+except:
+    print("An error has been accured. try again")
 
 
 
